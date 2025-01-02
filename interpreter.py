0ba903a57abf2ca45af5919bf18ede4b14f5d698
Meet Shah <meetshah9625@gmail.com>
2025-01-02 21:37:20 +0530

    Added another example file

--- interpreter.py
+++ interpreter.py
@@ -1,34 +1,37 @@
 import sys
 
-# read arguements
+# read arguments
 program_filepath = sys.argv[1]
 
-######################################
-#           Tokenize Program        #
-######################################
 
-# read the file
+###########################
+#     Tokenize Program
+###########################
+
+# read file lines
 program_lines = []
-with open(program_filepath, 'r') as program_file:
-    program_lines = [line.strip() for line in program_file.readlines()]
+with open(program_filepath, "r") as program_file:
+    program_lines = [
+        line.strip() 
+            for line in program_file.readlines()]
 
 program = []
 token_counter = 0
 label_tracker = {}
 for line in program_lines:
-    parts = line.split(" ")
+    parts  = line.split(" ")
     opcode = parts[0]
 
-    # check for empty lines
-    if opcode == " ":
+    # check for empty line
+    if opcode == "":
         continue
 
-    # Check if its a label
+    # check if its a label
     if opcode.endswith(":"):
         label_tracker[opcode[:-1]] = token_counter
         continue
 
-    # check opcode token
+    # store opcode token
     program.append(opcode)
     token_counter += 1
 
@@ -40,7 +43,7 @@ for line in program_lines:
         token_counter += 1
     elif opcode == "PRINT":
         # parse string literal
-        string_literal = " ".join(parts[1:])
+        string_literal = ' '.join(parts[1:])[1:-1]
         program.append(string_literal)
         token_counter += 1
     elif opcode == "JUMP.EQ.0":
@@ -54,16 +57,16 @@ for line in program_lines:
         program.append(label)
         token_counter += 1
 
-
-######################################
-#           Interpret Program        #
-######################################
+###########################
+#     Interpret Program
+###########################
 
 class Stack:
+
     def __init__(self, size):
         self.buf = [0 for _ in range(size)]
-        self.sp = -1 # Stack pointer
-    
+        self.sp    = -1
+
     def push(self, number):
         self.sp += 1
         self.buf[self.sp] = number
@@ -75,49 +78,44 @@ class Stack:
     
     def top(self):
         return self.buf[self.sp]
-    
+        
 
-pc = 0 # program counter
+
+pc = 0
 stack = Stack(256)
 
 while program[pc] != "HALT":
     opcode = program[pc]
     pc += 1
 
-    if "opcode" == "PUSH":
+    if opcode == "PUSH":
         number = program[pc]
         pc += 1
-        stack.push(number)
 
+        stack.push(number)
     elif opcode == "POP":
         stack.pop()
-    
     elif opcode == "ADD":
         a = stack.pop()
         b = stack.pop()
-        stack.push(a + b)
-    
+        stack.push(a+b)
     elif opcode == "SUB":
         a = stack.pop()
         b = stack.pop()
-        stack.push(b - a)
-    
+        stack.push(b-a)
     elif opcode == "PRINT":
         string_literal = program[pc]
         pc += 1
         print(string_literal)
-    
     elif opcode == "READ":
         number = int(input())
         stack.push(number)
-
     elif opcode == "JUMP.EQ.0":
-        number = stack.pop()
+        number = stack.top()
         if number == 0:
             pc = label_tracker[program[pc]]
         else:
             pc += 1
-    
     elif opcode == "JUMP.GT.0":
         number = stack.top()
         if number > 0:
